e8d9ae07f157c631081bdefff4f70e9625688ad1
Draincraft <hannes@famostermann.de>
2023-08-02 15:51:18 +0200

    Added Glowstone Evaporator to the Creative Tab

--- src/main/java/com/kokuxmilsch/celestialaltar/item/ModCreativeModeTabs.java
+++ src/main/java/com/kokuxmilsch/celestialaltar/item/ModCreativeModeTabs.java
@@ -14,7 +14,7 @@ public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CelestialAltar.MODID);
 
     public static final RegistryObject<CreativeModeTab> CELESTIAL_ALTAR_TAB = CREATIVE_MODE_TABS.register("celestial_altar_tab", () -> CreativeModeTab.builder()
-            .icon(() -> Items.SCULK_SHRIEKER.getDefaultInstance())
+            .icon(() -> ModBlocks.ALTAR.get().asItem().getDefaultInstance())
             .title(Component.translatable("creativetab.celestial_altar_tab"))
             .displayItems((pParameters, pOutput)-> {
                 pOutput.accept(ModItems.LIGHTNING_STEEL.get());
@@ -23,18 +23,18 @@ public class ModCreativeModeTabs {
                 pOutput.accept(ModItems.ENCHANTED_EYE_OF_ENDER.get());
                 pOutput.accept(ModBlocks.ALTAR.get());
                 pOutput.accept(ModBlocks.CELESTIAL_CRYSTAL.get());
+                pOutput.accept(ModBlocks.GLOW_STONE_EVAPORATOR.get());
 
                 pOutput.accept(Items.CRYING_OBSIDIAN);
                 pOutput.accept(Items.SCULK);
                 pOutput.accept(Items.DEEPSLATE_BRICK_WALL);
-                pOutput.accept(Items.RESPAWN_ANCHOR);
                 pOutput.accept(Items.END_ROD);
                 pOutput.accept(Items.SMOOTH_QUARTZ_STAIRS);
                 pOutput.accept(Items.EMERALD_BLOCK);
                 pOutput.accept(Items.GOLD_BLOCK);
                 pOutput.accept(Items.DIAMOND_BLOCK);
                 pOutput.accept(Items.BEACON);
-                pOutput.accept(Items.SCULK_SHRIEKER);
+                pOutput.accept(Items.ECHO_SHARD);
             })
             .build()
     );
