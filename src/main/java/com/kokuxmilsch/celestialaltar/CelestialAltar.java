a0e5bd1b09e56714c2cd847f193ec9444d15b9a1
Draincraft <hannes@famostermann.de>
2023-09-09 17:22:55 +0200

    Registered a BlockEntityRenderer for the Celestial Altar BlockEntity

--- src/main/java/com/kokuxmilsch/celestialaltar/CelestialAltar.java
+++ src/main/java/com/kokuxmilsch/celestialaltar/CelestialAltar.java
@@ -1,29 +1,23 @@
 package com.kokuxmilsch.celestialaltar;
 
 import com.kokuxmilsch.celestialaltar.block.ModBlocks;
+import com.kokuxmilsch.celestialaltar.block.entity.CelestialAltarBlockEntity;
 import com.kokuxmilsch.celestialaltar.block.entity.ModBlockEntities;
+import com.kokuxmilsch.celestialaltar.client.blockentityrenderer.CelestialAltarBlockEntityRenderer;
 import com.kokuxmilsch.celestialaltar.item.ModCreativeModeTabs;
 import com.kokuxmilsch.celestialaltar.item.ModItems;
 import com.kokuxmilsch.celestialaltar.recipe.ModRecipes;
-import com.kokuxmilsch.celestialaltar.screen.CelestialAltarScreen;
-import com.kokuxmilsch.celestialaltar.screen.ModMenuTypes;
+import com.kokuxmilsch.celestialaltar.client.screen.CelestialAltarScreen;
+import com.kokuxmilsch.celestialaltar.client.screen.ModMenuTypes;
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -33,9 +27,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -98,7 +90,13 @@ public class CelestialAltar
         public static void onClientSetup(FMLClientSetupEvent event)
         {
             ItemBlockRenderTypes.setRenderLayer(ModBlocks.ALTAR.get(), RenderType.cutout());
+            ItemBlockRenderTypes.setRenderLayer(ModBlocks.CELESTIAL_CRYSTAL.get(), RenderType.cutout());
             MenuScreens.register(ModMenuTypes.CELESTIAL_ALTAR_MENU.get(), CelestialAltarScreen::new);
+        }
+
+        @SubscribeEvent
+        public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
+            event.registerBlockEntityRenderer(ModBlockEntities.ALTAR_BLOCK_ENTITY.get(), CelestialAltarBlockEntityRenderer::new);
         }
     }
 }
